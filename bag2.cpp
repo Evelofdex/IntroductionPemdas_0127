a155d8862c4036171186bcc8b09c64f1e24e5dbd
Evelofdex <evelofdex@gmail.com>
2026-02-24 13:36:58 +0700

    menambahkan proses, input p, l, dan kalkulasi luas, juga output

--- bag2.cpp
+++ bag2.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int p, l, luas;
 
 int main(){
-
+    //input panjang
+    cout << "masukan panjang: ";
+    cin >> p;
+    //input lebar
+    cout << "masukan lebar: ";
+    cin >> l;
+    //kalkuklasi luas
+    luas = p * l;
+    //output hasil luas
+    cout << "Hasilnya = " << luas << endl;
+    cout << "sama sama";
+    //beres
 }
